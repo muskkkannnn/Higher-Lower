93bd84acebe14f2689adac98d8969e0779256736
CodeSandbox <support@codesandbox.io>
2024-10-07 02:16:51 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,58 +1,48 @@
-from game_data import data
+from game_data import data                                          #import ASCII art
 from art import logo, vs
 import random
-#import ASCII art
 print(logo)
 
 # Choose 2 random objects from data and Print both the statements.
-compare_1 = random.choice(data)
-
-def comparison():
+def comparison(compare_1):
     print(f"Compare A: {compare_1['name']}, a {compare_1['description']}, from {compare_1['country']}.")
-
-    # Insert 'vs.' ASCII art in between.
-    print(vs)
+    print(vs)                                                       # Insert 'vs.' ASCII art in between.
 
     compare_2 = random.choice(data)
     print(f"Against B: {compare_2['name']}, a {compare_2['description']}, from {compare_2['country']}.")
 
     # Input answer from user
     user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-    
-    return compare_1, compare_2, user_choice
 
-compare_1, compare_2, user_choice = comparison() 
+    return compare_2, user_choice
 
-# Input answer from user
-# user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-
-#'if-else' statement. Choose & Print another random object if the user choice is right. Else, Game Over.
-if user_choice == "A":
-    user_choice = compare_1
-    other_choice = compare_2
-elif user_choice == "B":
-    user_choice = compare_2
-    other_choice = compare_1
-# else:
-#     print("NO such choice.")
+compare_1 = random.choice(data)
+compare_2, user_choice = comparison(compare_1)              #Input compare_1 value, Catching compare_2 & user_choice values
 
 game_continue = True
 score = 0
 
-while game_continue == True:
-    if user_choice['follower_count'] > other_choice['follower_count']:
+#'if-else' statement. Choose & Print another random object if the user choice is right. Else, Game Over.
+while game_continue:
+    if user_choice == "A":
+        chosen_option = compare_1
+        other_option = compare_2
+    elif user_choice == "B":
+        chosen_option = compare_2
+        other_option = compare_1
+    else:
+        print("Invalid choice.")
+        break
+    
+    if chosen_option['follower_count'] > other_option['follower_count']:
         score += 1
         print(f"You're Right! Current Score: {score}\n")
+        
         compare_1 = compare_2
-        comparison()
+        compare_2, user_choice = comparison(compare_1)
     else:
         game_continue = False
-        # print(f"Sorry, That's Wrong. Final Score: {score}")
+        print(f"Sorry, That's Wrong. Final Score: {score}")
 
-    # compare_1 = compare_2
-    # comparison()
-
-print(f"Sorry, That's Wrong. Final Score: {score}")
-
-# Track Score (+1) for right answer. 
-# Display Score when Game Over.    
+        # Track Score (+1) for right answer. 
+        # Display Score when Game Over.    
